1272ab62d58cc7e38de577779c26f0b772435773
Pablo Almeida <pabloalmeidadasilva@eaportal.org>
2024-08-07 20:45:13 -0300

    atualização do dia 07/08

--- faq/faq.js
+++ faq/faq.js
@@ -1,18 +1,11 @@
-const botaoabrir = document.querySelector("header > button")
-const botaofechar = document.querySelector("header nav.mobile button")
-
-const body = document.querySelector("body")
-const nav = document.querySelector("header nav.mobile")
-
-botaoabrir.addEventListener('click', abrirMenu)
-botaofechar.addEventListener('click', fecharMenu)
-
-function abrirMenu() {
-    body.classList.add("tela-escurecer")
-    nav.classList.add("abrir")
+function home() {
+    window.location.href = "../home.html";
 }
 
-function fecharMenu() {
-    body.classList.remove("tela-escurecer")
-    nav.classList.remove("abrir")
+function guia() {
+    window.location.href = "../../home.html";
+}
+
+function avaliacao() {
+    window.location.href = "../avaliacao/avaliacao.html";
 }
