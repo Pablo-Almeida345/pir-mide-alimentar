57a9cb7df7345e39df3d7ff7a13cfa3acbf1abcb
Pablo Almeida <pabloalmeidadasilva@eaportal.org>
2024-08-15 17:54:30 -0300

    adicionado os links do youtube do saiba mais

--- 2piramide/carboidratos/carboidratos.js
+++ 2piramide/carboidratos/carboidratos.js
@@ -1,3 +1,7 @@
 function seta() {
     window.location.href = "../piramide.html";
 }
+
+function mais(){
+    window.location.href = "https://www.youtube.com/watch?feature=shared&v=s3dmb-zOX7c";
+}
--- 2piramide/gorduras/gorduras.js
+++ 2piramide/gorduras/gorduras.js
@@ -1,3 +1,7 @@
 function seta() {
     window.location.href = "../piramide.html";
 }
+
+function mais(){
+    window.location.href = "https://www.youtube.com/watch?feature=shared&v=kE46ae34IR0";
+}
