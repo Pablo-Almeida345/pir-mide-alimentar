f6f6362a7e4e07e9b33d69d442400ace9cc11982
Pablo Almeida <pabloalmeidadasilva@eaportal.org>
2024-09-11 20:32:54 -0300

    alteração dos textos de quiz para memoria

--- script.js
+++ script.js
@@ -25,8 +25,8 @@ planejamento.addEventListener('click', () => {
     window.location.href = 'https://docs.google.com/forms/d/e/1FAIpQLScIwhAJgX330aeNJ3HpVC5sYQgKR8aS2ktE1IOHyYi2vq2Qyg/viewform?embedded=true';
 });
 
-function quiz() {
-    window.location.href = "quiz/quiz.html";
+function memoria() {
+    window.location.href = "memoria/memoria.html";
 }
 
 function receita() {
