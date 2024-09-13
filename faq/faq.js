52e7d556918a37784e837f0e5fbba540641a9dae
Pablo Almeida <pabloalmeidadasilva@eaportal.org>
2024-09-12 22:48:23 -0300

    Adicionado o guia no faq

--- faq/faq.js
+++ faq/faq.js
@@ -1,3 +1,25 @@
+const BotaoAbrir = document.querySelector ("div > .guia")
+const BotaoFechar = document.querySelector ("header nav button")
+
+const body = document.querySelector("body")
+const nav = document.querySelector("header nav")
+
+BotaoAbrir.addEventListener('click', abrirMenu )
+BotaoFechar.addEventListener('click', fecharMenu )
+
+function abrirMenu() {
+    body.classList.add("escurecer")
+    nav.classList.add("abrir")
+}
+
+function fecharMenu(){
+    body.classList.remove("escurecer")
+    nav.classList.remove("abrir")
+}
+
+
+
+
 function home() {
     window.location.href = "../../index.html";
 }
