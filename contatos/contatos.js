6076a564d064fa3521b31a89320c24468b8a5b3d
Pablo Almeida <pabloalmeidadasilva@eaportal.org>
2024-09-12 23:36:13 -0300

    Adicionado a piramide o guia

--- contatos/contatos.js
+++ contatos/contatos.js
@@ -1,5 +1,20 @@
-function home() {
-    window.location.href = "../index.html";
+const BotaoAbrir = document.querySelector ("div > .guia")
+const BotaoFechar = document.querySelector ("header nav button")
+
+const body = document.querySelector("body")
+const nav = document.querySelector("header nav")
+
+BotaoAbrir.addEventListener('click', abrirMenu )
+BotaoFechar.addEventListener('click', fecharMenu )
+
+function abrirMenu() {
+    body.classList.add("escurecer")
+    nav.classList.add("abrir")
+}
+
+function fecharMenu(){
+    body.classList.remove("escurecer")
+    nav.classList.remove("abrir")
 }
 
 
